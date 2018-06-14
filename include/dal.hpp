1abc9fdd0cf84003d4ac2e76877d7ba6cfcaf55f
Felipe Ramos <felipecramos@ufrn.edu.br>
2018-06-13 21:40:44 -0300

    Main structure made

--- include/dal.hpp
+++ include/dal.hpp
@@ -1,6 +1,60 @@
 #ifndef _DAL_HPP_
 #define _DAL_HPP_
 
+class DAL{
+	protected:
+		using Key = int;			// Key alias
+		using Data = std::string;	// Data Alias
+		struct NodeAL{				// Node alias, containing the pair key-data
+			Key id;
+			Data info;
+		};
+
+		static const int SIZE=50;	// Default size of the list
+		int mi_Lenght;				// Actual size of the list
+		int mi_Capacity;			// Actual capacity of the list
+		NodeAL *mpt_Data;			// Data vector, dinamically allocated
+
+		int _search( const Key & _x ) const;	// Auxiliar search method
+
+	public:
+		/** Default constructor of DAL class. */
+		DAL( int _MaxSz = SIZE );
+		virtual ~DAL(){ delete [] mpt_Data; };
+
+		/** Removes an item from the list. */
+		bool remove( const Key & _x, Data & _s );
+
+		/** Search for an item in the list. */
+		bool search( const Key & _x, Data & _s ) const;
+
+		/** Insert an item on the list. */
+		bool insert( const Key & _newKey, const Data & _newInfo );
+
+		/** Retrieves the minimal key from the dictionary */
+		Key min() const;
+
+		/** Retrieves the maximum key from the dictionary */
+		Key max() const;
+
+		/** Retrieves on `_y` the next key to `_x`, if exists (true) */
+		bool sucessor( const Key & _x, Key & _y ) const;
+
+		/** Retrieves on `_y` the prev key to `_x`, if exists (true) */
+		bool predecessor( const Key & _x, Key & _y ) const;
+
+		// sends back to the output stream an ASCII representation for the list
+		inline friend
+			std::ostream &operator<<( std::ostream & _os, const DAL & _oList ){
+				_os << "[ ";
+				for( int i = 0; i < _oList.mi_Lenght; i++ ){
+					_os << "{id: " << _oList.mpt_Data[i].id << ", info: "
+						<< _oList.mpt_Data[i].info << "}  ";
+				}
+				_os << "]";
+				return _os;
+			}
+};
 
 #include "dal.inl"
 #endif
--- include/dsal.hpp
+++ include/dsal.hpp
@@ -1,5 +1,33 @@
 #ifndef _DSAL_HPP_
 #define _DSAL_HPP_
+#include "dal.hpp"
+
+class DSAL : public DAL{
+	public:
+		/** Default constructor of DSAL class. */
+		DSAL( int _MaxSz ) : DAL( _MaxSz ){ /* empty */ };
+		virtual ~DSAL(){ /* Empty */ };
+
+		/** Removes an item from the list. */
+		bool remove( const Key & _x, Data & _s );
+
+		/** Insert a item on the list. */
+		bool insert( const Key & _newKey, const Data & _newInfo );
+
+		/** Retrieves the minimal key from the dictionary. */
+		Key min() const;
+
+		/** Retrieves the maximum key from the dictionary. */
+		Key max() const;
+
+		/** Retrieves on `_y` the next key to `_x`, if exists (true) */
+		bool sucessor( const Key & _x, Key & _y ) const;
+
+		/** Retrieves on `_y` the prev key to `_x`, if exists (true) */
+		bool predecessor( const Key & _x, Key & _y ) const;
+	private:
+		int _search( const Key & _x ) const;
+};
 
 #include "dsal.inl"
 #endif
